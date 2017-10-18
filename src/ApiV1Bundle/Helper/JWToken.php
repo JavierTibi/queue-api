aef763b4e54a52bc39dac0faaa0ed35295b020f6
Fausto Carrera <fcarrera@hexacta.com>
2017-10-18 17:46:36 -0300

    se agrega helper de tokens

---
+++ src/ApiV1Bundle/Helper/JWToken.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * JWToken class
+ * Docs: https://github.com/lcobucci/jwt/blob/3.2/README.md
+ */
+namespace ApiV1Bundle\Helper;
+
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\ValidationData;
+
+class JWToken
+{
+}
