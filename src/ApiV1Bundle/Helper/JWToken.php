c53f675fb9b1e86ae402a3c709f8cf9460fd6a09
Fausto Carrera <fcarrera@hexacta.com>
2017-10-20 14:11:27 -0300

    JWT class

--- src/ApiV1Bundle/Helper/JWToken.php
+++ src/ApiV1Bundle/Helper/JWToken.php
@@ -10,4 +10,17 @@ use Lcobucci\JWT\ValidationData;
 
 class JWToken
 {
+    public function __construct()
+    {
+        $this->domain = $_SERVER['SERVER_NAME'];
+    }
+
+    public function token()
+    {
+        $token = new Builder();
+        $token->setIssuer($this->domain);
+        $token->setAudience($this->domain);
+        $token->setId(); // the jti claim
+        return $token;
+    }
 }
